04f9b650a733eb1928de79505c4192a9c47d4c04
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2025-05-28 14:18:21 +0700

    Update CustomerModel.php

--- app/Models/customers/CustomerModel.php
+++ app/Models/customers/CustomerModel.php
@@ -13,21 +13,9 @@ class CustomerModel extends Model
     protected $casts = [
         'customer_files' => 'array',
     ];
-     protected $fillable = [
-        'customer_name',
-        'customer_taxid',
-        'customer_branch',
-        'customer_branch_name',
-        'customer_address_number',
-        'customer_address_district',
-        'customer_address_amphur',
-        'customer_address_province',
-        'customer_address_zipcode',
-        'customer_files',
-        'created_by',
-     ];
+    protected $fillable = ['customer_name', 'customer_taxid', 'customer_branch', 'customer_branch_name', 'customer_address_number', 'customer_address_district', 'customer_address_amphur', 'customer_address_province', 'customer_address_zipcode', 'customer_files', 'created_by'];
 
-     public function contracts()
+    public function contracts()
     {
         return $this->hasMany(contractModel::class, 'customer_id');
     }
@@ -36,9 +24,7 @@ class CustomerModel extends Model
         return $this->belongsTo(GlobalSetValueModel::class, 'customer_branch');
     }
     public function latestContract()
-{
-    return $this->hasOne(contractModel::class, 'customer_id')->latestOfMany('contract_end_date');
-}
-
-    
+    {
+        return $this->hasOne(contractModel::class, 'customer_id')->latestOfMany('contract_end_date');
+    }
 }
